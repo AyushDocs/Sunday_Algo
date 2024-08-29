2b2986469e16841c179a463452030b34928d3e96
AyushDocs <ayushalokdubey@gmail.com>
2024-08-29 22:10:17 +0530

    second commit

--- KruskalAlgorithm.java
+++ KruskalAlgorithm.java
@@ -1,116 +1,82 @@
 package Sunday_Algos;
-import java.util.*;
 
-class Edge implements Comparable<Edge> {
-    int src, dest, weight;
+import java.util.Arrays;
 
-    public int compareTo(Edge compareEdge) {
-        return this.weight - compareEdge.weight;
-    }
-};
-
-class Subset {
-    int parent, rank;
-};
-
+//gets mst 
 public class KruskalAlgorithm {
-
-    int no_of_vertices, no_of_edges;
-    Edge[] edges;
-
-    KruskalAlgorithm(int v, int e) {
-        no_of_vertices = v;
-        no_of_edges = e;
-        edges = new Edge[no_of_edges];
-        for (int i = 0; i < e; ++i)
-            edges[i] = new Edge();
-    }
-    int find(Subset[] subsets, int i) {
-        if (subsets[i].parent != i)
-            subsets[i].parent = find(subsets, subsets[i].parent);
-        return subsets[i].parent;
+    
+    // A utility function to find set of an element i (uses path compression technique)
+    static int find(int parent[], int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent, parent[i]);
     }
 
-    void union(Subset[] subsets, int x, int y) {
-        int xroot = find(subsets, x);
-        int yroot = find(subsets, y);
+    // A utility function to do union of two subsets
+    static void union(int parent[], int rank[], int x, int y) {
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
 
-        if (subsets[xroot].rank < subsets[yroot].rank)
-            subsets[xroot].parent = yroot;
-        else if (subsets[xroot].rank > subsets[yroot].rank)
-            subsets[yroot].parent = xroot;
+        if (rank[rootX] < rank[rootY])
+            parent[rootX] = rootY;
+        else if (rank[rootX] > rank[rootY])
+            parent[rootY] = rootX;
         else {
-            subsets[yroot].parent = xroot;
-            subsets[xroot].rank++;
+            parent[rootY] = rootX;
+            rank[rootX]++;
         }
     }
-    void kruskalMST() {
-        Edge[] result = new Edge[no_of_vertices];
-        int e = 0;
-        int i = 0;
-        for (i = 0; i < no_of_vertices; ++i)
-            result[i] = new Edge();
 
-        Arrays.sort(edges);
+    // The main function to construct MST using Kruskal's algorithm
+    static void kruskalMST(int[][] graph) {
+        int n = graph.length;
+        int[][] result = new int[n - 1][3]; // To store the resultant MST
+        int[] parent = new int[n];
+        int[] rank = new int[n];
 
-        Subset[] subsets = new Subset[no_of_vertices];
-        for (i = 0; i < no_of_vertices; ++i)
-            subsets[i] = new Subset();
+        Arrays.fill(rank,0);
+        
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
 
-        for (int v = 0; v < no_of_vertices; ++v) {
-            subsets[v].parent = v;
-            subsets[v].rank = 0;
-        }
+        int[][] edges = new int[n * (n - 1) / 2][3]; // To store all edges
+        int k = 0;
 
-        i = 0;
-
-        while (e < no_of_vertices - 1) {
-            Edge next_edge = edges[i++];
-
-            int x = find(subsets, next_edge.src);
-            int y = find(subsets, next_edge.dest);
-
-            if (x != y) {
-                result[e++] = next_edge;
-                union(subsets, x, y);
+        // Store all edges from the adjacency matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (graph[i][j] != 0) {
+                    edges[k][0] = i;
+                    edges[k][1] = j;
+                    edges[k][2] = graph[i][j];
+                    k++;
+                }
             }
         }
 
-        System.out.println("Following are the edges in the constructed MST");
-        int minimumCost = 0;
-        for (i = 0; i < e; ++i) {
-            System.out.println(result[i].src + " -- " + result[i].dest + " == " + result[i].weight);
-            minimumCost += result[i].weight;
+        // Sort all edges in non-decreasing order of their weight
+        Arrays.sort(edges, (a, b) -> a[2] - b[2]);
+
+        int e = 0; // Index used to store edges in result[]
+        k = 0; // Index used to iterate over sorted edges
+
+        while (e < n - 1 && k < edges.length) {
+            int[] next_edge = edges[k++];
+
+            int x = find(parent, next_edge[0]);
+            int y = find(parent, next_edge[1]);
+
+            // If including this edge doesn't cause cycle, include it in the result
+            if (x != y) {
+                result[e++] = next_edge;
+                union(parent, rank, x, y);
+            }
         }
-        System.out.println("Minimum Cost Spanning tree : " + minimumCost);
-    }
 
-    public static void main(String[] args) {
-
-        int V = 4;
-        int E = 5;
-        KruskalAlgorithm graph = new KruskalAlgorithm(V, E);
-
-        graph.edges[0].src = 0;
-        graph.edges[0].dest = 1;
-        graph.edges[0].weight = 10;
-
-        graph.edges[1].src = 0;
-        graph.edges[1].dest = 2;
-        graph.edges[1].weight = 6;
-
-        graph.edges[2].src = 0;
-        graph.edges[2].dest = 3;
-        graph.edges[2].weight = 5;
-
-        graph.edges[3].src = 1;
-        graph.edges[3].dest = 3;
-        graph.edges[3].weight = 15;
-
-        graph.edges[4].src = 2;
-        graph.edges[4].dest = 3;
-        graph.edges[4].weight = 4;
-
-        graph.kruskalMST();
+        // Print the resultant MST
+        System.out.println("Edges in the constructed MST:");
+        for (int i = 0; i < e; i++) {
+            System.out.println(result[i][0] + " -- " + result[i][1] + " == " + result[i][2]);
+        }
     }
 }
--- ShortestDistanceBetweenTwoNodes.java
+++ ShortestDistanceBetweenTwoNodes.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class ShortestDistanceBetweenTwoNodes {
     public static int getShortestDistance(int graph[][],int startingNode,int endingNode){
         if(startingNode==endingNode)return 0;
-
+        //bfs
         int n=graph.length;
         boolean visited[]=new boolean[n];
-        int distance[]=new int[n];
+        int distance[]=new int[n];//new field
         Queue<Integer> q=new LinkedList<>();
 
         visited[startingNode]=true;
@@ -19,6 +19,7 @@ public class ShortestDistanceBetweenTwoNodes {
                 if(graph[ele][i]!=0 && visited[i]==false){
                     q.add(i);
                     visited[ele]=true;
+                    //new part in our code
                     distance[i]=distance[ele]+1;
                     if(i==endingNode)return distance[i];
                 }
